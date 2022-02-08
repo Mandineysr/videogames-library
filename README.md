08467df2b7f41871a2eab141b77ab2c75debb048
Mandineysr <mandine-ysr@outlook.fr>
2022-02-08 17:37:14 +0100

    repo propre

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Interface de gestion de jeux vidéo
+
+Je souhaite gérer moi-même mon nombre insolent de jeux vidéo en ma possession, et ce, depuis mon premier Tetris sur GameBoy.
+Du coup, petit projet fait avec PHP et Bootstrap.
+Je peuc ajouter des jeux avec différents champs à remplir obligatoires et ils sont listés. 
+Je peux les trier par éditeur et par noms.
+
+Capture d'écran :
+
+<img src="./capture.png">
---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+
+// Inclusion du fichier s'occupant de la connexion à la DB
+require __DIR__.'/inc/db.php';
+
+// Initialisation de variables
+$videogameList = array();
+$platformList = array();
+$name = '';
+$editor = '';
+$release_date = '';
+$platform = '';
+
+$errorList = [];
+
+// Si le formulaire a été soumis
+if (!empty($_POST)) {
+    // Récupération des valeurs du formulaire dans des variables
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+
+    $editor = isset($_POST['editor']) ? $_POST['editor'] : '';
+    $release_date = isset($_POST['release_date']) ? $_POST['release_date'] : '';
+
+    // intval transforme une chaine de caractère en valeur numérique
+    $platform = isset($_POST['platform']) ? intval($_POST['platform']) : 0;
+    
+    if (empty($name)) {
+        $errorList [] = 'Le nom est obligatoire';
+    }
+
+    if (empty($editor)) {
+        $errorList [] = 'L\'éditeur est obligatoire';
+    }
+
+    if (empty($platform)) {
+        $errorList [] = 'La plateforme est obligatoire';
+    }
+    
+    // Insertion en DB du jeu video
+    $insertQuery = "
+        INSERT INTO videogame (name, editor, release_date, platform_id)
+        VALUES ('{$name}', '{$editor}', '{$release_date}', {$platform})
+    ";
+
+    if (empty($errorList)) {
+        // Si on a pas de données vides...alors on fait notre insert
+
+        $isInserted = $pdo->exec($insertQuery);
+    
+        if ($isInserted !== false) {
+            // On fait une redirection propre pour éviter le bug de la double soumission de données        
+            // On repart d'une feuille blanche, sans les données qui ont été soumises avant
+            header('Location: index.php');
+            exit();
+        } else {
+            $errorList [] = 'Erreur de sauvegarde';
+        }
+    }
+}
+
+// Liste des consoles de jeux
+$requete = $pdo->query('SELECT id, name FROM platform');
+
+$platformList = $requete->fetchAll(PDO::FETCH_KEY_PAIR);
+
+$sql = '
+    SELECT * FROM videogame
+';
+
+// Si un tri a été demandé, on réécrit la requête
+
+$hasOrder = false;
+if (!empty($_GET['order'])) {
+
+    // Le tri est en place, on va pouvoir afficher le bouton "Annuler le trie"
+    $hasOrder = true;
+
+    // Récupération du tri choisi
+    // La fonction trim supprimer les espaces avant et après une chaine de caractère
+    $order = trim($_GET['order']);
+    if ($order == 'name') {
+        $sql = '
+            SELECT * FROM videogame ORDER BY name;
+        ';
+    }
+    else if ($order == 'editor') {
+        $sql = '
+            SELECT * FROM videogame ORDER BY editor ASC;
+        ';
+    }
+}
+
+// Execution de la requete
+$pdoStatement = $pdo->query($sql);
+
+// Récupération du résultat de la requete
+$videogameList = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+
+// Inclusion du fichier s'occupant d'afficher le code HTML
+// Je fais cela car mon fichier actuel est déjà assez gros
+require __DIR__.'/view/videogame.php';
